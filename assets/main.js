1349760d23a82d322dd5142ba83ccf716330b22e
VAB\GiseleLuz <giseleluz@aguiabranca.com.br>
2024-05-13 11:10:42 -0300

    ajuste

--- assets/main.js
+++ assets/main.js
@@ -11,4 +11,20 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     window.addEventListener('scroll', animateModalidade);
+
+   
+     function checkScreenWidth(mediaQuery) {
+        if (mediaQuery.matches) {
+         
+          document.querySelector('.bannerPrincipal').src = '/assets/images/bannermobile.png';
+        } else {
+          document.querySelector('.bannerPrincipal').src = 'assets/images/bannertorneio.png';
+        }
+      }
+  
+      checkScreenWidth(window.matchMedia('(max-width: 590px)'));
+  
+      window.addEventListener('resize', function() {
+        checkScreenWidth(window.matchMedia('(max-width: 590px)'));
+      });
 });
